847e82871c611a732468d69f867b67435a737526
Home <none@gmail.com>
2023-09-08 14:38:04 +0300

    add_swagger

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Options;
 using Microsoft.Identity.Web;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,14 +14,50 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "swaggerAADdemo", Version = "v1" });
+    c.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+    {
+        Description = "OAuth2.0 Auth Code with PKCE",
+        Name = "oauth2",
+        Type = SecuritySchemeType.OAuth2,
+        Flows = new OpenApiOAuthFlows
+        {
+            AuthorizationCode = new OpenApiOAuthFlow
+            {
+                AuthorizationUrl = new Uri("https://login.microsoftonline.com/0a4d4529-3bd8-41d2-bdbe-aa4634a1752b/oauth2/v2.0/authorize"),
+                TokenUrl = new Uri("https://login.microsoftonline.com/0a4d4529-3bd8-41d2-bdbe-aa4634a1752b/oauth2/v2.0/token"),
+                Scopes = new Dictionary<string, string>
+                {
+                    { "api://658c4be3-619c-4146-adf4-fd93f71deed9/access_as_user", "read the api" }
+                }
+            }
+        }//Serhii.Riabushenko@infopulse.com
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "oauth2" }
+            },
+            new[] { "api://658c4be3-619c-4146-adf4-fd93f71deed9/access_as_user" }
+        }
+    });
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "swaggerAADdemo v1");
+        c.OAuthClientId("658c4be3-619c-4146-adf4-fd93f71deed9");
+        c.OAuthUsePkce();
+    });
 }
 
 app.UseHttpsRedirection();
